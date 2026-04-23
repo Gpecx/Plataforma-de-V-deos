c149926bdc358a0c9f3b2efd034b1dae8da0778e
Gpecx <danielsiqueira2027@gmail.com>
2026-04-23 11:25:40 -0300

    fix: unifica CSP para imagens e PIX e resolve

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "img-src 'self' data: https://sandbox.asaas.com https://www.asaas.com https://images.unsplash.com;",
+            value: "img-src 'self' data: blob: https://sandbox.asaas.com https://www.asaas.com https://firebasestorage.googleapis.com https://*.googleusercontent.com https://images.unsplash.com *;",
           },
         ],
       },
